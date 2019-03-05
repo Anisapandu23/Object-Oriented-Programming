2743d88a155020cd297f2301c6bdc591445c44bf
Anisapandu23 <48240506+Anisapandu23@users.noreply.github.com>
2019-03-05 17:45:46 +0700

    Update Anisa-Pandu_17090076_4D.java

--- Anisa-Pandu_17090076_4D.java
+++ Anisa-Pandu_17090076_4D.java
@@ -2,7 +2,7 @@ package object_oriented_programming;
 
 public class printt {
     public static void main(String[] args){
-         system.out.println("Selamat datang di Object Oriented Programing"
+         system.out.println("Selamat datang di Mata Kuliah : Object Oriented Programing"
                + "Nama : Anisa Pandu Sabillah \n"
                + "Nim : 17090076");
     }
